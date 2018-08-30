9bb285e2640c05e1e0334b45a1f0c018484ca9b8
Jonathanmp <Jonathan_m.p@hotmail.com>
2018-08-30 02:32:55 -0700

    .
    .

---
+++ CafetoteAPP/CafetoteAPP/Models/SplashScreenModel.cs
@@ -0,0 +1,25 @@
+﻿
+using Foundation;
+
+namespace CafetoteAPP.Models
+{
+    public class SplashScreenModel : Observable
+    {
+       
+        string _version;
+        public string Version
+        {
+            get
+            {
+
+                return NSBundle.MainBundle.InfoDictionary["CFBundleVersion"].ToString();
+            }
+            set
+            {
+                _version = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+    }
+}
---
+++ CafetoteAPP/CafetoteAPP/ViewModels/SplashScreenViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CafetoteAPP.Models;
+namespace CafetoteAPP.ViewModels
+{
+
+
+public Models.SplashScreenModel _model { get; set;}
+ 
+    public class SplashScreenViewModel
+    {
+        public SplashScreenViewModel()
+        {
+			_model ?? new SplashScreenModel();
+           
+        }
+    }
+}
